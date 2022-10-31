99ffed0ff657608f5b2a6ec6a631502c1af18f5a
Lucas Santos Souza <62265013+lucas-souza19@users.noreply.github.com>
2022-10-31 20:27:42 -0300

    Alterando PORT

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-const port = process.eventNames.PORT || 3000;
+const port = process.env.PORT || 3000;
 require('../startup/prod')(app);
 
 app.listen(port, () => {
